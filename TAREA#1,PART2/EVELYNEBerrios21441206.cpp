c35eebe3313ae450dc7ab18ec4b31624416cefec
EvelyneBerrios <evelyne.berrios@hotmail.com>
2016-02-22 16:21:10 -0600

    holaedwin

---
+++ TAREA#1,PART2/EVELYNEBerrios21441206.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int c1=0, c2=0, c3=0;
+    int num;
+    int c=0;
+    while(c<5)
+    {
+        cout<<"Ingresar numero...:";
+        cin>>num;
+        if(num<0)
+        {
+            num = num * (-1); //num=*1
+        }
+        if((num>=0)and (num<=50))
+            c1++;
+        else if((num>=51)and(num<=80))
+            c2++;
+        else
+            c3++;
+    c++;
+    }
+    cout<<"Contador entre 0-50  "<<c1<<"\n";
+    cout<<"Contador entre 51-80 "<<c2<<"\n";
+
+    cout<<"Contador entre >80   "<<c3<<"\n";
+
+
+    return 0;
+}
